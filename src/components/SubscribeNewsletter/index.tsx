7a23b3e01fc4546a65633cf5d9fa1590b6d99f00
fladeia <ladeiadev@gmail.com>
2022-09-21 10:24:08 -0300

    test add formik

--- src/components/SubscribeNewsletter/index.tsx
+++ src/components/SubscribeNewsletter/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Envelope } from 'phosphor-react'
 import heroImage from '../../assets/imagem-hero.png'
 import { Card } from '../Card'
@@ -7,6 +8,12 @@ import plantImage from '../../assets/card-small-image-1.png'
 
 export function SubscribeNewsletter() {
   const plants = ['Ajuga reptans', 'Cordyline fruticosa', 'Crassula ovata', 'Cyperus rotundus', 'Delairea odorata', 'Datura metel']
+  const [email, setEmail] = useState('')
+
+  function handleClick(e: React.SyntheticEvent) {
+    console.log(email)
+    setEmail('')
+  }
 
   return (
     <>
@@ -18,9 +25,11 @@ export function SubscribeNewsletter() {
             Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.
           </p>
           <div className="subscribe w-full h-[75px] mt-9 flex  items-center shadow-lg shadow-gray-400 ">
-            <Envelope size={24} className='w-16 text-placeholder text-gray opacity-50' />
-            <input type="email" name="" id="" placeholder='Insira seu e-mail' className='flex-1 outline-none text-gray opacity-50' />
-            <button className='h-full px-10 bg-yellow text-white'>Assinar Newsletter</button>
+            <label htmlFor="email">
+              <Envelope size={24} className='w-16 text-placeholder text-gray opacity-50' />
+            </label>
+            <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder='Insira seu e-mail' className='flex-1 outline-none text-gray opacity-50' />
+            <button type='submit' className='h-full px-10 bg-yellow text-white' onClick={handleClick}>Assinar Newsletter</button>
           </div>
         </section>
         <section className="right w-[618px]">
